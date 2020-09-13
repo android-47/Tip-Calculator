59b192195f550af1a5165f723683ce654292878f
android-47 <jx.garcia@outlook.com>
2020-09-13 00:51:59 -0700

    Added an automatic keyboard when segment is placed on Custom.

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,13 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var counter = 0
-
+    // Text fields
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var customField: UITextField!
     @IBOutlet weak var splitField: UITextField!
-
+    // Labels
     @IBOutlet weak var segOneLabel: UILabel!
     @IBOutlet weak var segTwoLabel: UILabel!
     @IBOutlet weak var segThreeLabel: UILabel!
@@ -24,13 +22,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var customLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    
+    // Other controls
     @IBOutlet weak var currencyButton: UIButton!
     @IBOutlet weak var payButton: UIButton!
     @IBOutlet weak var outletSwitch: UISwitch!
     @IBOutlet weak var splitStepper: UIStepper!
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    var counter = 0
+    var segOne = 0.0
+    var segTwo = 0.0
+    var segThree = 0.0
+    var custom = 0.0
+    var bill = 0.0
+    var split = 0.0
+    var tip = 0.0
+    var total = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,9 +46,11 @@ class ViewController: UIViewController {
         billField.becomeFirstResponder()
     }
     
-    @IBAction func splitAction(_ sender: Any) {
-        splitField.text = String(format: "%.0f", splitStepper.value)
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setBackground()
     }
+    
     
     func roundCorners () {
         segOneLabel.layer.masksToBounds = true
@@ -56,42 +65,6 @@ class ViewController: UIViewController {
         segThreeLabel.layer.cornerRadius = 15.0
         customLabel.layer.cornerRadius = 15.0
     }
-
-
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        setBackground()
-    }
-
-    @IBAction func currencyButton(_ sender: UIButton) {
-        self.sortCurrency()
-
-    }
-    
-
-    func sortCurrency(){
-        let arraySize = 4
-        
-        if counter < arraySize {
-            counter = counter + 1
-        } else {
-            counter = 0
-        }
-        
-    }
-    
-    @IBAction func darkAction(_ sender: Any) {
-        if outletSwitch.isOn == true {
-            overrideUserInterfaceStyle = .dark
-            print ("overide to dark")
-        } else {
-            overrideUserInterfaceStyle = .light
-            print ("override to light")
-        }
-    }
-    
-
-    
     
     func setBackground () {
         switch traitCollection.userInterfaceStyle {
@@ -104,73 +77,62 @@ class ViewController: UIViewController {
                 outletSwitch.setOn(true, animated:true)
             }
         case .unspecified:
-            print ("unspecififed")
+            print ("unspecified")
         }
     }
     
-
+    @IBAction func darkAction(_ sender: Any) {
+        if outletSwitch.isOn == true {
+            overrideUserInterfaceStyle = .dark
+        } else {
+            overrideUserInterfaceStyle = .light
+        }
+    }
     
+    
+    @IBAction func segmentAction(_ sender: Any) {
+        view.endEditing(true)
+        let billPercentages = [0.1, 0.15, 0.2, Double(customField.text!) ?? 0]
+        if (billPercentages[tipControl.selectedSegmentIndex]) == billPercentages[3]{
+            customField.becomeFirstResponder()
+        }
+    }
+    
+    
+    @IBAction func splitAction(_ sender: Any) {
+        resignFirstResponder()
+        view.endEditing(true)
+        splitField.text = String(format: "%.0f", splitStepper.value)
+    }
     
     @IBAction func onTap(_ sender: Any) {
+        resignFirstResponder()
         view.endEditing(true)
-
     }
     
     
+    @IBAction func currencyButton(_ sender: UIButton) {
+        resignFirstResponder()
+        view.endEditing(true)
+        let arraySize = 4
+        if counter < arraySize{
+            counter = counter + 1
+        } else {
+            counter = 0
+        }
+        self.sortCurrency()
+    }
+    
 
     
-    @IBAction func calculateTip(_ sender: Any) {
-        let bill = Double(billField.text!) ?? 0
-        let split = splitStepper.value
-        var tip = 0.0
-        var custom = 0.0
-        
-        
-        let billPercentages = [0.1, 0.15, 0.2, Double(customField.text!) ?? 0]
 
-        
-        
-        
-        
-        if (billPercentages[tipControl.selectedSegmentIndex]) == billPercentages[3]{
-            tip = billPercentages[3]
-            custom = tip
-            //customLabel.text = String(format: "$%.2f", custom)
-        } else {
-            tip = bill * billPercentages[tipControl.selectedSegmentIndex]
-        }
-        
-        let segOne = bill * billPercentages[0]
-        let segTwo = bill * billPercentages[1]
-        let segThree = bill * billPercentages[2]
-        
-        var tipPercent = 100*(tip/bill)
-        let tipString = "% Tip"
-        var testString = String(format: "%.0f", tipPercent)
-        
-        if testString == "inf" || testString == "nan"{
-            testString = "0"
-            print ("test")
-        }
-        tipPercentLabel.text = testString + tipString
-        
-        
-        let total = (bill + tip)/split
-        
-        
-        
-        var index = 1
-        
-        
-        
-        
-        let testArray = [segOne, segTwo, segThree, custom, bill, tip, total]
+    func sortCurrency(){
         var labelArray = [segOneLabel, segTwoLabel, segThreeLabel, customLabel, billLabel, tipLabel, totalLabel]
-        
-        
+        let testArray = [segOne, segTwo, segThree, custom, bill, tip, total]
         let currencyLocale = ["en_US", "fr_FR", "de_DE", "pt_BR", "en_UK"]
         let currencyCountries = ["US", "FRA", "GER", "BRIT", "UK"]
         let currency = currencyLocale[counter]
+        
         currencyButton.setTitle(currencyCountries[counter], for: .normal)
         
         var z = 0
@@ -178,33 +140,41 @@ class ViewController: UIViewController {
             let currencyFormatter = NumberFormatter()
             currencyFormatter.usesGroupingSeparator = true
             currencyFormatter.numberStyle = .currency
-            
             //currencyFormatter.locale = Locale.current
-            
             currencyFormatter.locale = Locale(identifier: currency)
-            //print ("CURRENCY", currency, counter)
-            
-                
             let priceString = currencyFormatter.string(from: NSNumber(value: testArray[z]))!
-            //print("price", priceString) // Displays $9,999.99 in the US locale
-            
             labelArray[z]?.text = priceString
             z = z+1
         }
-        
-
-        
-        //segOneLabel.text = String(format: "$%.2f", segOne)
-        //                                                          add those comments
-        //segTwoLabel.text = String(format: "$%.2f", segTwo)
-        //segThreeLabel.text = String(format: "$%.2f", segThree)
-        //billLabel.text = priceString
-        //tipLabel.text = String(format: "$%.2f", tip)
-        //totalLabel.text = String(format: "$%.2f", total)
-        
-
     }
     
 
     
+    @IBAction func calculateTip(_ sender: Any) {
+        let billPercentages = [0.1, 0.15, 0.2, Double(customField.text!) ?? 0]
+        bill = Double(billField.text!) ?? 0
+        split = splitStepper.value
+
+        if (billPercentages[tipControl.selectedSegmentIndex]) == billPercentages[3]{
+            tip = billPercentages[3]
+            custom = tip
+        } else {
+            tip = bill * billPercentages[tipControl.selectedSegmentIndex]
+        }
+        
+        segOne = bill * billPercentages[0]
+        segTwo = bill * billPercentages[1]
+        segThree = bill * billPercentages[2]
+        
+        var tipPercent = 100*(tip/bill)
+        let tipString = "% Tip"
+        var testString = String(format: "%.0f", tipPercent)
+        if testString == "inf" || testString == "nan"{
+            testString = "0"
+            print ("test")
+        }
+        tipPercentLabel.text = testString + tipString
+        total = (bill + tip)/split
+        sortCurrency()
+    }
 }
